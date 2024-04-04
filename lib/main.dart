1e4013c89c8649d602787c8cba46402ce8ed0358
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-04-04 11:31:03 -0300

    feat: add initial testing firebase operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // criar coleção formandos no Firebase
+  final CollectionReference _formandos =
+      FirebaseFirestore.instance.collection('formandos');
+  // adicionar documentos para teste
+  // _formandos.add({'nome': 'Bernardo', 'fone': '991690252'});
+  // _formandos.add({'nome': 'Camila', 'fone': '991690252'});
+  // atualizar documento com base no ID
+  _formandos.doc('ah0h5bL7Rj6BXknx3bLw').update({'fone': '111111'});
+  // deletar documento com base no ID
+  // _formandos.doc('ah0h5bL7Rj6BXknx3bLw').delete();
+
+  // fazer busca pelos documentos
+  QuerySnapshot snapshot = await _formandos.get();
+  snapshot.docs.forEach((element) {
+    print(element.data().toString());
+  });
+
   runApp(const MyApp());
 }
 
