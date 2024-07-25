a04524d45e633dc693625f55331c3a5e4d093eeb
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-07-25 10:01:03 -0300

    feat: add menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat/menu.dart';
 import 'package:chat/screens/chat_screen.dart';
 import 'package:chat/screens/map_geo.dart';
 import 'package:flutter/material.dart';
@@ -41,8 +42,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MapSample(),
-      //ChatScreen(),
+      home: NavigationOptions(),
     );
   }
 }
---
+++ lib/menu.dart
@@ -0,0 +1,54 @@
+import 'package:chat/screens/map_geo.dart';
+import 'package:flutter/material.dart';
+//import 'screens/lista.dart';
+import 'screens/chat_screen.dart';
+
+class NavigationOptions extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NavigationOptionsState();
+  }
+}
+
+class _NavigationOptionsState extends State<NavigationOptions> {
+  int paginaAtual = 0;
+  PageController? pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: paginaAtual);
+  }
+
+  setPaginaAtual(pagina) {
+    setState(() {
+      paginaAtual = pagina;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pc,
+        children: [
+          ChatScreen(),
+          MapSample(),
+        ],
+        onPageChanged: setPaginaAtual,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: paginaAtual,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.location_on), label: "Mapa")
+        ],
+        onTap: (pagina) {
+          pc?.animateToPage(pagina,
+              duration: const Duration(milliseconds: 400), curve: Curves.ease);
+        },
+        backgroundColor: Colors.grey[200],
+      ),
+    );
+  }
+}
